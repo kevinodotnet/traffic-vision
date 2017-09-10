dc9a2b9bc4b2d802786af501246bbfe22b725f8c
Kevin O'Donnell <kevino@kevino.net>
2017-09-10 15:16:58 -0400

    GUID

--- www/upload-video.php
+++ www/upload-video.php
@@ -1,5 +1,11 @@
 <?php
 include('include.php');
+
+function GUID()
+{
+    return sprintf('%04X%04X%04X%04X%04X%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
 top();
 ?>
 <div class="row">
